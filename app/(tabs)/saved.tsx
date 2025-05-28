8e1251e6ea53acd5d70c582efe622f45c72205a9
Bryan Juanacio <bryanjuanacio@Mac-mini-de-Bryan.local>
2025-05-28 14:28:58 -0500

    feat: init navigation

---
+++ app/(tabs)/saved.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function Saved() {
+  return (
+    <View>
+      <Text>Saved</Text>
+    </View>
+  );
+}
+
+export default Saved;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function MovieDetails() {
+  return (
+    <View>
+      <Text>MovieDetails</Text>
+    </View>
+  );
+}
+
+export default MovieDetails;
